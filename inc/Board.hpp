54073ab1a7dceb4124c7334007954957ee45c94f
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-06 14:36:51 +0200

    Move board over from hpp only to hpp and cpp

---
+++ inc/Board.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "Entity.hpp"
+#include "Texture.hpp"
+#include "Timer.hpp"
+
+class Board {
+  public:
+    Board();
+    ~Board();
+    void ConvertSketch();
+    void CopyBoard(unsigned char ActualMap[]);
+    void ResetPosition(Entity& ThisEntity);
+    void SetScore();
+    void SetHighScore();
+    bool IsExtraLife();
+    void IncreaseLives();
+    void DecreaseLives();
+    void ScoreIncrease(unsigned short Scorer);
+    unsigned char GetLives();
+    void Draw(unsigned char ActualMap[], Timer MapAnimationTimer);
+
+  private:
+    LTexture MapTexture;
+    LTexture PelletTexture;
+    LTexture EnergizerTexture;
+    LTexture DoorTexture;
+    LTexture LivesTexture;
+    LTexture ScoreWordTexture;
+    LTexture ScoreTexture;
+    LTexture HighScoreWordTexture;
+    LTexture HighScoreTexture;
+    unsigned char NumericBoard[BoardHeight * BoardWidth];
+    unsigned int Score;
+    bool IsExtra;
+    char Lives;
+};
