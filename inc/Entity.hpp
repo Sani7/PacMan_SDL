fc3f197772043278624fc72166d0885c9b6a071d
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-06 14:36:27 +0200

    Move entity over from hpp only to hpp and cpp

---
+++ inc/Entity.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "Position.hpp"
+#include "main.hpp"
+
+class Entity : public Position {
+  public:
+    enum EntityType { ePacMan, eBlinky, eInky, ePinky, eClyde, Noone };
+    enum BlockType { Wall, Door, Pellet, Energizer, Nothing };
+
+  public:
+    Entity(EntityType MyIdentity);
+    unsigned char GetIdentity();
+    unsigned char GetSpeed();
+    unsigned char GetDirection();
+    unsigned char GetFacing();
+    bool IsAlive();
+    void ModSpeed(unsigned char NewSpeed);
+    void ModDirection(unsigned char NewDirection);
+    void ModFacing(unsigned char NewFacing);
+    void ModLifeStatement(bool NewLifeStatement);
+    void GetPossiblePosition(short& x, short& y, Position::Direction mover);
+    void CharBoardPos(unsigned char SideDir, Position& BoardPos, float cell_x, float cell_y);
+    bool WallCollision(short x, short y, unsigned char ActualMap[], bool CanUseDoor = 0);
+    void Move(unsigned char mover);
+    void CheckWrap();
+    bool IsColliding(Position other);
+
+  private:
+    unsigned char Identity;
+    unsigned char Speed;
+    unsigned char Direction;
+    unsigned char Facing;
+    bool LifeStatement;
+};
