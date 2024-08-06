c26f54b78ae54524418bb54e86fcb88b5f7145ad
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-06 14:12:06 +0200

    Move timer over from hpp only to hpp and cpp

---
+++ inc/Timer.hpp
@@ -0,0 +1,22 @@
+// Copied from https://lazyfoo.net/tutorials/SDL/ and adapted to my project
+#pragma once
+#include "stdint.h" 
+
+class Timer {
+  public:
+    Timer();
+    void Start();
+    void Reset();
+    void Restart();
+    void Pause();
+    void Unpause();
+    uint32_t GetTicks();
+    bool isStarted();
+    bool isPaused();
+
+  private:
+    uint32_t StartTicks;  // Ticks since start
+    uint32_t PausedTicks; // Ticks since stop
+    bool Paused;
+    bool Started;
+};
