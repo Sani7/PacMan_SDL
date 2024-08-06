41a2a08a0abe6efb78eaa7b0ac6ae0c623294023
Sander Speetjens <sander.speetjens@gmail.com>
2024-08-06 14:32:22 +0200

    Move position over from hpp only to hpp and cpp

---
+++ inc/Position.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class Position {
+  public:
+    enum Direction { Right, Up, Left, Down, Nowhere };
+
+  public:
+    Position(short x = 0, short y = 0);
+    short GetX();
+    short GetY();
+    Position GetPos();
+    void ModX(short NewX);
+    void ModY(short NewY);
+    void ModCoords(short NewX, short NewY);
+    void ModPos(Position NewPos);
+    bool operator==(Position other) { // see, i'm an expert...?
+        return this->x == other.GetX() && this->y == other.GetY();
+    }
+
+  private:
+    short x, y;
+};
